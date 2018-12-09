f4c545a7d7600f45669ec7d8fa5e4cdb7efcd2a5
radeth <123radeth@gmail.com>
2018-12-09 03:03:42 +0100

    fibonnaci added

--- src/components/Fibonacci.js
+++ src/components/Fibonacci.js
@@ -1,13 +1,45 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 
-class Fibonacci extends Component{
-    render(){
-        return(
+class Fibonacci extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            fibonacciNumbers: []
+        }
+
+        this.calculate = this.calculate.bind(this);
+    }
+    calculate(e) {
+        e.preventDefault();
+        let fibonacciNumbersArray = this.numbers(this._input.value)
+        console.log(fibonacciNumbersArray)
+        this.setState({fibonacciNumbers:fibonacciNumbersArray})
+    }
+    numbers(border) {
+        let fibonacciNumbers = [1,2];
+        for (let i=2; i <= border; i++) {
+          if(i===fibonacciNumbers[fibonacciNumbers.length-1]+fibonacciNumbers[fibonacciNumbers.length-2]){
+              fibonacciNumbers.push(i)
+          }
+        }
+        return fibonacciNumbers
+
+    }
+    render() {
+        return (
             <div>
                 <h1>Fibonacci</h1>
+                <form onSubmit={this.calculate}>
+                    <input ref={(value) => this._input = value} />
+                    <button>press</button>
+                </form>
+                <ul>
+                    {this.state.fibonacciNumbers.map((val) => <li>{val}</li>)}
+                </ul>
             </div>
-            
+
         );
 
     }
